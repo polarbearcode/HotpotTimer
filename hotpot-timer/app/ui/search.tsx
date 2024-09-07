3550436fe5274064d8eff3e25127deca9b055ca3
Benny Chen <benny.jw.chen@gmail.com>
2024-09-07 14:52:30 -0700

    added search bar working on suggestions

--- hotpot-timer/app/ui/search.tsx
+++ hotpot-timer/app/ui/search.tsx
@@ -2,6 +2,7 @@
  
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
  
 export default function Search({ placeholder }: { placeholder: string }) {
 
@@ -9,8 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     const pathName = usePathname();
     const { replace } = useRouter();
 
-    function handleSearch(term: string) {
-        const params = new URLSearchParams(searchParams);
+    const handleSearch = useDebouncedCallback((term) => {
+      const params = new URLSearchParams(searchParams);
         console.log(term);
 
         if (term) {
@@ -20,23 +21,33 @@ export default function Search({ placeholder }: { placeholder: string }) {
         }
 
         replace(`${pathName}?${params.toString()}`);
-    }
-
+    }, 300);
  
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get('ingredient')?.toString()}
-      />
-      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-    </div>
+    <>
+      <div className="relative flex flex-1 flex-shrink-0">
+        <label htmlFor="search" className="sr-only">
+          Search
+        </label>
+        <input
+          className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+          placeholder={placeholder}
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+          defaultValue={searchParams.get('ingredient')?.toString()}
+        />
+        <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      
+        <div className="absolute top-full mt-1 w-full p-2 bg-white border border-gray-200">
+          <ul>
+            <li>Mushroom</li>
+            <li>Tofu</li>
+          </ul>
+        </div>
+      </div>
+
+    </>
+    
   );
 }
