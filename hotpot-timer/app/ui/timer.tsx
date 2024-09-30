2f98aeb8f6d7f51e8c9c7519f99612a3be543403
Benny Chen <benny.jw.chen@gmail.com>
2024-09-30 16:35:23 -0700

    add start button

--- hotpot-timer/app/ui/timer.tsx
+++ hotpot-timer/app/ui/timer.tsx
@@ -1,21 +1,29 @@
 'use client'
 import React, { useEffect, useState } from "react";
+import { start } from "repl";
 
-export default function Timer({ cookTime }  : {cookTime: number}) {
+export default function Timer({ cookTime}  : {cookTime: number}) {
   const [time, setTime] = useState(cookTime * 60);
+  const [startTimer, setStartTimer] = useState(false);
 
   useEffect(() => {
     let timer = setInterval(() => {
       setTime((time) => {
-        if (time === 0) {
-          clearInterval(timer);
-          return 0;
-        } else return time - 1;
+        if (startTimer) {
+          if (time === 0) {
+            clearInterval(timer);
+            return 0;
+          } else {
+            return time - 1;
+          }
+        } else {
+          return time;
+        }
       });
     }, 1000);
 
     return () => clearInterval(time); // have to return a function so use () =>.
-  }, []);
+  }, [startTimer]);
 
   return (
     <div>
@@ -23,6 +31,11 @@ export default function Timer({ cookTime }  : {cookTime: number}) {
         Time left: {`${Math.floor(time / 60)}`.padStart(2, '0')}:
         {`${time % 60}`.padStart(2, '0')}
       </p>
+      <button onClick={() => 
+        {setStartTimer(true);}
+      }>
+        Start
+        </button>
     </div>
   );
 }
