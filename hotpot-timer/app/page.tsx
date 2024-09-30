c32fa88ea952658b9cdaab71bc0b066c4d725a89
Benny Chen <benny.jw.chen@gmail.com>
2024-09-30 16:13:23 -0700

    search

--- hotpot-timer/app/page.tsx
+++ hotpot-timer/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
-import Search from '@/app/ui/search'
+import Search from '@/app/ui/search';
+import Table from '@/app/ui/table';
 import { Suspense } from "react";
+import { Ingredient } from "./lib/definitions";
 
-export default function Home() {
+export default async function Home() {
+
+  const ingredientList: Ingredient[] = [{name: "Mushroom", cook_time: 2, cook_descr: "long"}];
   return (
    <>
     <div className="flex justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
@@ -13,11 +17,14 @@ export default function Home() {
         <p className="text-lg text-gray-700 dark:text-gray-300">
             Track the ingredients in your hotpot
         </p>
-        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <div className="relative mt-4 flex items-center justify-between gap-2 md:mt-8">
           <Suspense>
             <Search placeholder="placeholder" />
           </Suspense>
 
+          <div className='absolute top-10'>
+            <Table ingredientsList={ingredientList}></Table>
+          </div>
          </div>
       </div>
     </div>
