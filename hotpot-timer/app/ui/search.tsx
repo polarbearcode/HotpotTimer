351988e82b0e79a864ba9ec06d433de15af6707e
Benny Chen <benny.jw.chen@gmail.com>
2024-10-02 21:34:11 -0700

    take out suggestions

--- hotpot-timer/app/ui/search.tsx
+++ hotpot-timer/app/ui/search.tsx
@@ -4,6 +4,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 import { useState } from 'react';
+import SearchSuggestions from '@/app/ui/search-suggestions';
 
 import { fetchIngredientName } from '../lib/data';
  
@@ -19,7 +20,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
-      console.log(term);
 
       if (term) {
           params.set('ingredient', term);
@@ -49,7 +49,6 @@ export default function Search({ placeholder }: { placeholder: string }) {
           defaultValue={searchParams.get('ingredient')?.toString()}
         />
         <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-      
       </div>
 
     </>
