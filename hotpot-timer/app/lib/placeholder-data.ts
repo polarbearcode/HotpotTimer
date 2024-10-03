692bd8d6efc62e3dbe75c404b72df528f69c459a
Benny Chen <benny.jw.chen@gmail.com>
2024-10-02 21:36:09 -0700

    place holder data for seeding

---
+++ hotpot-timer/app/lib/placeholder-data.ts
@@ -0,0 +1,15 @@
+export const ingredients = [
+    {
+        name: "mushroom",
+        cook_time: 2,
+        cook_descr: "long"
+    },
+
+    {
+        name: "tofu",
+        cook_time: 5,
+        cook_descr: "long"
+
+    }
+]
+
