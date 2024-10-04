35fb9d17cddca76af705c545e72d439834d45af1
Benny Chen <benny.jw.chen@gmail.com>
2024-10-04 12:13:36 -0700

    remove async keyword

--- hotpot-timer/app/ui/search-suggestions.tsx
+++ hotpot-timer/app/ui/search-suggestions.tsx
@@ -1,13 +1,39 @@
+
+import { Dispatch, SetStateAction } from "react";
 import { fetchIngredientSuggestions, fetchIngredientName } from "../lib/data";
+import { Ingredient } from "../lib/definitions";
 
-export default async function SearchSuggestions({query}: {query: string}) {
-    const suggestions = await fetchIngredientSuggestions(query);
+export default function SearchSuggestions({query, ingredientsList, setAddedIngredients}:
+     {query: string,
+      ingredientsList: Ingredient[],
+      setAddedIngredients: Dispatch<SetStateAction<Ingredient[]>>;
+      }) {
 
+    
+    function handleOnClick(value : {
+        name: string; cook_time: number, cook_descr: 'long' | 'short' | 'medium'
+    }) {
+        ingredientsList.push({name: value.name, cook_time: value.cook_time, cook_descr: value.cook_descr});
+        setAddedIngredients(ingredientsList);
+    }
+
+    var filteredList: Ingredient[] = [];
+
+    if (query) {
+        filteredList = ingredientsList.filter(item => {
+            item.name.startsWith(query);
+        })
+    } 
+
+    console.log("here");
+
+    
 
     return (
         <>
-            {suggestions.map((item, key) => {
-                <p>{item}</p>;
+            {filteredList.map((item, key) => {
+                    <p>{item.name}</p>;
+                    <button>Add</button>              
             })}
         </>
     )
