3b8349931a80084c1892de8d2a80bf0a9e674ba9
Benny Chen <benny.jw.chen@gmail.com>
2024-10-04 12:13:16 -0700

    parent comp for suggestions and tabe

---
+++ hotpot-timer/app/ui/ingredients-display.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useState } from "react";
+import { Ingredient } from "../lib/definitions";
+import SearchSuggestions from "./search-suggestions";
+import Table from "./table";
+
+export default function IngredientsDisplay({query, ingredientsList} : {query: string, ingredientsList: Ingredient[]}) {
+    const placeholderTableData: Ingredient[] = [{name: "Food", cook_time: 1, cook_descr: "long"}];
+    const [addedIngredients, setAddedIngredients] = useState<Ingredient[]>(placeholderTableData);
+    return (
+        <>
+        <div className="relative">
+          <SearchSuggestions 
+            query={query} 
+            ingredientsList={ingredientsList}
+            setAddedIngredients={setAddedIngredients}
+          >  
+          </SearchSuggestions>
+          </div>
+          <div className='relative'>
+            {<Table ingredientsList={placeholderTableData}></Table>}
+          </div>
+        </>
+    )
+
+}
