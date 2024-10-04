41c46cee53ea235ef454505868d34c2522016bf7
Benny Chen <benny.jw.chen@gmail.com>
2024-10-04 13:05:10 -0700

    fix SQL for fetching to fetch all cols

--- hotpot-timer/app/lib/data.ts
+++ hotpot-timer/app/lib/data.ts
@@ -2,9 +2,9 @@ import { sql } from '@vercel/postgres';
 import { Ingredient } from './definitions';
 
 export async function fetchIngredientName() {
-    const data = await sql<Ingredient>`SELECT name FROM ingredients;`
+    const data = await sql<Ingredient>`SELECT * FROM ingredients;`
     const nameList = data.rows.map(item => {
-        return item.name;
+        return {name: item.name, cook_time: item.cook_time, cook_descr: item.cook_descr};
     })
 
     return nameList;
@@ -12,7 +12,6 @@ export async function fetchIngredientName() {
 }
 
 export async function fetchIngredientSuggestions(query:string) {
-    console.log(query);
     const data = await sql<Ingredient>`
         SELECT name FROM ingredients
         WHERE (lower(name) LIKE lower(${query + '%'}));
